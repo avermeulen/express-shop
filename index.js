ada7af45118336709c76acf4a53e6243872d0874
avermeulen <andre.vermeulen@gmail.com>
2018-08-02 18:08:18 +0200

    removed working code

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
-const Shop = require('./shop');
-const shop = Shop();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -18,24 +16,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', function(req, res){
-    res.render('home', shop.settings());
+    res.render('home');
 });
 
 app.post('/buy', function(req, res){
-    let qty = Number(req.body.qty);
-    shop.buy(qty)
-    res.redirect('/');
+    
 });
 
 app.post('/set-price', function(req, res){
-    let price = parseFloat(req.body.price);
-    shop.setPrice(price);
-    res.redirect('/');
+    
 });
 
 app.post('/clear', function(req, res){
-    shop.clear();
-    res.redirect('/');
+    
 });
 
 app.listen(3010, function(){
