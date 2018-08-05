401c8d3950a47371776762988113a62a9498047d
avermeulen <andre.vermeulen@gmail.com>
2018-08-05 16:31:58 +0200

    changes made in the first video

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 
+let currentPrice = 0;
+let grandTotal = 0;
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -16,7 +19,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', function(req, res){
-    res.render('home');
+    res.render('home', {
+        currentPrice,
+        grandTotal
+    });
+});
+
+app.post('/set-price', function(req, res){
+    let price = req.body.price;
+    currentPrice = price;
+    res.redirect('/');
+});
+
+app.post('/buy', function(req, res){
+    let qty = req.body.qty;
+    grandTotal += (currentPrice * qty);
+    res.redirect('/');
 });
 
 app.listen(3010, function(){
